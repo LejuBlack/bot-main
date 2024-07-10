eb7fb2a4576f83e88fbfa80f5b67139cfab11be8
LejuBlack <fatemehkhakpoor07@gmail.com>
2024-07-10 11:04:45 -0400

    Your commit message

--- adder.bat
+++ adder.bat
@@ -1,5 +1,10 @@
-git pull origin main
+cd "C:\Users\RedSmrt\Desktop\New folder (3)"
+git config --global user.name "LejuBlack"
+git config --global user.email "fatemehkhakpoor07@gmail.com"
+git remote remove origin
+git remote add origin https://github.com/LejuBlack/bot-main.git
 git add .
-git commit -m "Resolved merge conflicts"
-git push -f origin main
+git commit -m "Your commit message"
+git push -u origin main
+
 pause
