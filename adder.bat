7e5b7ecb40eac87a5c9f56d31025dd19f288f239
LejuBlack <fatemehkhakpoor07@gmail.com>
2024-07-10 11:03:20 -0400

    Resolved merge conflicts

--- adder.bat
+++ adder.bat
@@ -1,12 +1,5 @@
-git config --global user.name "LejuBlack"
-git config --global user.email "fatemehkhakpoor07@gmail.com"
-
-git remote remove origin
-git remote add origin https://github.com/LejuBlack/bot-discord.git
-
+git pull origin main
 git add .
-git commit -m "Initial commit"
-git branch -M main
-git push -u origin main
-
+git commit -m "Resolved merge conflicts"
+git push -f origin main
 pause
